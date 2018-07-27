39b47dc039fd65ca1e29f656c70364a6376fde91
Alberto <ing_aperezr@yahoo.com>
2018-07-27 13:10:12 -0500

    Salida JSON

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
 http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
 
-        res.write('Hola Mundo');
+        let salida = {
+            nombre: 'Alberto',
+            cel: 'Xperia X',
+            url: req.url
+        };
+        //res.write('Hola Mundo');
+        res.write(JSON.stringify(salida));
         res.end();
     })
     .listen(8080);
