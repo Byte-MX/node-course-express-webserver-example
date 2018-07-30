4c07930bafca74c2530499282c2be5aa04d97024
Alberto <ing_aperezr@yahoo.com>
2018-07-30 15:45:33 -0500

    Lección 67 completa (Recién instalado HBS)

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
+
+// Express HBS engine
+app.set('view engine', 'hbs');
+
+
 app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'Alberto',
+        anio: new Date().getFullYear()
+    });
+    /*
     //res.send('Hola Mundo')
     let salida = {
         nombre: 'Alberto',
@@ -11,10 +23,12 @@ app.get('/', (req, res) => {
     //res.write('Hola Mundo');
     res.send(salida) //Equivale a: res.write(JSON.stringify(salida));
         // Y a: res.end();
+    */
 });
 app.get('/data', (req, res) => {
     res.send('Hola data');
 });
+
 app.listen(3000, () => {
     console.log('Escuchando peticiones en puerto 3000');
 });
